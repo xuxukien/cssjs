a2eea0ebd0eb8d14e2c89924cebfabf5ed7dbdf6
xuxukien <huynhkien92@gmail.com>
2017-06-04 17:11:11 +0700

    Add files via upload

--- redirectcopy.js
+++ redirectcopy.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='http://www.topella.com/'>http://www.topella.com/"+document.location.pathname+"</a>";
+    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='http://xukien.com/'>http://xukien.com/"+document.location.pathname+"</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
